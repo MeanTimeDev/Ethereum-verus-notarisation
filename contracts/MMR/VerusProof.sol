74741030974ceae3a7fc3811b2bcd52ff7790438
James Russell <james@iMac.broadband>
2021-09-10 15:45:48 +0100

    Update VerusProof

--- contracts/MMR/VerusProof.sol
+++ contracts/MMR/VerusProof.sol
@@ -23,6 +23,75 @@ contract VerusProof{
         verusNotarizer = VerusNotarizer(notarizerAddress);   
     }
 
+
+    function checkProof(bytes32 hashToProve, VerusObjects.CTXProof[] memory _branches) public returns(bytes32){
+        //loop through the branches from bottom to top
+        bytes32 hashInProgress = hashToProve;
+        for(uint i = 0; i < _branches.length; i++){
+            hashInProgress = checkBranch(hashInProgress,_branches[i].proofSequence);
+        }
+        return hashInProgress;
+    }
+
+    function checkBranch(bytes32 _hashToCheck,VerusObjects.CMerkleBranch memory _branch) public returns(bytes32){
+        
+        require(_branch.nIndex >= 0,"Index cannot be less than 0");
+        require(_branch.branch.length > 0,"Branch must be longer than 0");
+        uint branchLength = _branch.branch.length;
+        bytes32 hashInProgress;
+        bytes memory joined;
+        hashInProgress = blake2b.bytesToBytes32(abi.encodePacked(_hashToCheck));
+        uint hashIndex = _branch.nIndex;
+        
+       for(uint i = 0;i < branchLength; i++){
+            if(hashIndex & 1 > 0){
+                require(_branch.branch[i] != _hashToCheck,"Value can be equal to node but never on the right");
+                //join the two arrays and pass to blake2b
+                joined = abi.encodePacked(_branch.branch[i],hashInProgress);
+            } else {
+                joined = abi.encodePacked(hashInProgress,_branch.branch[i]);
+            }
+            hashInProgress = blake2b.createHash(joined);
+            hashIndex >>= 1;
+        }
+
+        return hashInProgress;
+
+    }
+    //roll through each proveComponents
+    function proveComponents(VerusObjects.CReserveTransferImport memory _import) public returns(bytes32 txRoot){
+        
+        bytes32 hashInProgress;
+        
+        if (_import.partialtransactionproof.components.length > 0)
+        {   
+             hashInProgress = blake2b.createHash(_import.partialtransactionproof.components[0].elVchObj);
+            if (_import.partialtransactionproof.components[0].elType == 1 )
+            {
+                txRoot = checkProof(hashInProgress,_import.partialtransactionproof.components[0].elProof);           
+            }
+        }
+        
+        for(uint i = 1; i < _import.partialtransactionproof.components.length; i++){
+            hashInProgress = blake2b.createHash(_import.partialtransactionproof.components[i].elVchObj);
+           if(txRoot != checkProof(hashInProgress,_import.partialtransactionproof.components[i].elProof)){
+               txRoot = 0x0000000000000000000000000000000000000000000000000000000000000000;
+               break;
+           }          
+        }
+        
+        return txRoot;
+        
+    }
+    
+    function proveTransaction(VerusObjects.CReserveTransferImport memory _import) public returns(bytes32 stateRoot){
+        bytes32 txRoot = proveComponents(_import);
+        if(txRoot == 0x0000000000000000000000000000000000000000000000000000000000000000) return txRoot;
+        
+        return checkProof(txRoot,_import.partialtransactionproof.txproof);
+    }
+    
+
     function proveTransaction(bytes32 mmrRootHash,bytes32 notarisationHash,bytes32[] memory _transfersProof,uint32 _hashIndex) public returns(bool){
         if (mmrRootHash == predictedRootHash(notarisationHash,_hashIndex,_transfersProof)) return true;
         else return false;
